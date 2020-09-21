79fb0e9ca6142b6f9e3985122c2690d50530e03d
Hamza-Rashed <hamzarashed503@gmail.com>
2020-09-21 14:53:26 +0300

    qefewrf

--- js/app.js
+++ js/app.js
@@ -29,17 +29,17 @@ let mid_image = document.getElementById('mid-image');
 let right_image = document.getElementById('right-image');
 let imagesSection = document.getElementById('images-Section');
 let result = document.getElementById('result');
-
+let Vote_arr = [];
 function Vote(name,ext) {
   this.name = name;
   this.ext = ext;
   this.image_path = `img/${name}.${ext}`;
   this.votes = 0;
   this.times_showes = 0;
-  Vote.all.push(this);
+  Vote_arr.push(this);
 
 }
-Vote.all = [];
+
 
 for (let i = 0; i < images.length; i++) {
   new Vote(images[i].name,images[i].ext);
@@ -48,9 +48,9 @@ for (let i = 0; i < images.length; i++) {
 function render() {
 
   while ( left_side === mid_side || right_side === mid_side || left_side === right_side) {
-    var right_side = Vote.all[randomNumber(0, Vote.all.length - 1)];
-    var left_side = Vote.all[randomNumber(0, Vote.all.length - 1)];
-    var mid_side = Vote.all[randomNumber(0, Vote.all.length - 1)];
+    var right_side = Vote_arr[randomNumber(0, Vote_arr.length - 1)];
+    var left_side = Vote_arr[randomNumber(0, Vote_arr.length - 1)];
+    var mid_side = Vote_arr[randomNumber(0, Vote_arr.length - 1)];
   }
 
   left_side.times_showes++;
@@ -69,10 +69,10 @@ function render() {
 function show_result() {
   let ulEl = document.createElement('ul');
   result.appendChild(ulEl);
-  for (let i = 0; i < Vote.all.length; i++) {
+  for (let i = 0; i < Vote_arr.length; i++) {
     let liEl = document.createElement('li');
     ulEl.appendChild(liEl);
-    liEl.innerHTML = `<span>${images[i].name}</span> had<span> ${Vote.all[i].votes}</span> votes and was shown <span>${Vote.all[i].times_showes}</span> times`;
+    liEl.innerHTML = `<span>${images[i].name}</span> had<span> ${Vote_arr[i].votes}</span> votes and was shown <span>${Vote_arr[i].times_showes}</span> times`;
   }
 }
 render();
@@ -80,15 +80,16 @@ render();
 function end_event(event) {
   if (counter_end_event <= 25) {
     if (event.target.id !== 'imagesSection') {
-      for (let i = 0; i < Vote.all.length; i++) {
-        if (event.target.title === Vote.all[i].name) {
-          Vote.all[i].votes++;
+      for (let i = 0; i < Vote_arr.length; i++) {
+        if (event.target.title === Vote_arr[i].name) {
+          Vote_arr[i].votes++;
         }
       }
       render();
     }
     if (counter_end_event === 26) {
       show_result();
+      create_chart();
     }
   }
 }
@@ -98,4 +99,53 @@ imagesSection.addEventListener('click', end_event);
 // helper function
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function create_chart(){
+
+  let name = [];
+  let views = [];
+  let votes = [];
+  for (let info in Vote_arr) {
+    name.push(Vote_arr[info].name);
+    views.push(Vote_arr[info].times_showes);
+    votes.push(Vote_arr[info].votes);
+    console.log(Vote_arr)
+  }
+
+var ctx = document.getElementById('myChart').getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: name,
+      datasets: [{
+        label: 'votes',
+        data: votes,
+        backgroundColor: 'yellow',
+        borderColor: 'black',
+        borderWidth: 1.0,
+        barPercentage: 1.0
+      }, {
+        label: 'views',
+        data: views,
+        backgroundColor: 'blue',
+        borderColor: 'red',
+        borderWidth: 1.0,
+        borderPercentage: 1.0
+      }]
+    },
+
+    option: {
+      scales: {
+        xAxis: [{ stacked: true }],
+        yAxis: [{
+          stacked: true,
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 }
